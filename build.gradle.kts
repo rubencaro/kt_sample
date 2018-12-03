cd511b0ab3ec8a92673d6bd9758cef629875e11d
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-12-03 19:17:36 +0100

    Building example

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ * This generated file contains a sample Kotlin application project to get you started.
+ */
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM
+    kotlin("jvm") version "1.3.10"
+
+    // Apply the application to add support for building a CLI application
+    application
+}
+
+repositories {
+    // Use jcenter for resolving your dependencies.
+    jcenter()
+}
+
+dependencies {
+    // Use the Kotlin JDK 8 extended standard library
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Use the Kotlin JUnit 5 integration
+    testImplementation(kotlin("test-junit5"))
+}
+
+application {
+    // Define the main class for the application (note the ending 'Kt')
+    mainClassName = "kt_sample.AppKt"
+}
